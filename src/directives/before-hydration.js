7b98c0be784f6b1e5a38dea623ad9b335f7eb514
Nitay Neeman <nitayneeman@users.noreply.github.com>
2025-05-20 11:19:19 +0300

    implement

--- src/directives/before-hydration.js
+++ src/directives/before-hydration.js
@@ -1,4 +1,9 @@
 export default async (load, opts, el) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
   const hydrate = await load();
   await hydrate();
 };
